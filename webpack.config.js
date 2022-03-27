fbc1c99c44ae5b56dc4e8d8a7d10e88cb05dc4ce
Sebastian Pape <s@pape.io>
2022-03-27 08:37:30 +0200

    v0.9.12: webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,48 @@
+const webpack = require('webpack');
 const path = require('path');
-const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
 
 const umd = {
+  target: "web",
+  mode: "production",
   output: {
     filename: 'index.js',
     library: 'CoinbaseWalletSdk',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist/umd')
   },
-  node: {
-    Buffer: true,
-    "crypto": require.resolve("crypto-browserify")
+  resolve: {
+    fallback: {
+      // stream: require.resolve('stream-browserify'),
+    },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+       Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
 }
 
 const esm = {
+  target: "web",
+  mode: "production",
+  experiments: {
+    outputModule: true,
+  },
   output: {
     filename: 'index.js',
-    library: 'CoinbaseWalletSdk',
-    libraryTarget: 'var',
+    libraryTarget: 'module',
     path: path.resolve(__dirname, 'dist/esm')
   },
-  plugins: [
-    new EsmWebpackPlugin()
-  ],
-  node: {
-    Buffer: true,
-    "crypto": require.resolve("crypto-browserify")
+  resolve: {
+    fallback: {
+      // stream: require.resolve('stream-browserify'),
+    },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+       Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
 }
 
 module.exports = [umd, esm]
