2f0078a5c06d70bb895410c4ea5a71ba558aac11
Sebastian Pape <0xneo11@gmail.com>
2023-12-31 11:48:59 +0100

    v3.8.1: export umd+esm

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,9 @@
 const path = require('path');
 
-module.exports = {
+// Base configuration
+const baseConfig = {
     // The entry point of your library
     entry: './src/index.js',
-
-    // Output configuration
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        library: {
-            type: 'module',
-        },
-    },
-
-    // Experiments for enabling ESM
-    experiments: {
-        outputModule: true,
-    },
 
     // Module and rules for handling JavaScript files
     module: {
@@ -44,13 +31,39 @@ module.exports = {
     // Mode (development or production)
     mode: 'production',
 
-    // Optional: Configuration for webpack-dev-server
-    devServer: {
-        static: path.join(__dirname, 'public'),
-        compress: true,
-        port: 9000,
-    },
-
     // Optional: Source map configuration for development
     devtool: 'source-map',
 };
+
+// Configuration for ESM output
+const esmConfig = {
+    ...baseConfig,
+    name: 'esm',
+    output: {
+        path: path.resolve(__dirname, 'dist/esm'),
+        filename: 'index.js',
+        library: {
+            type: 'module',
+        },
+    },
+    experiments: {
+        outputModule: true,
+    },
+};
+
+// Configuration for UMD output
+const umdConfig = {
+    ...baseConfig,
+    name: 'umd',
+    output: {
+        path: path.resolve(__dirname, 'dist/umd'),
+        filename: 'index.js',
+        library: {
+            name: 'CoinbaseWalletSdk',
+            type: 'umd',
+        },
+    },
+    // Remove the experiments field for UMD
+};
+
+module.exports = [esmConfig, umdConfig];
